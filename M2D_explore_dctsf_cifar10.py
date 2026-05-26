ddd057a876d88f55119d41ef0ae4b51887a978e3
a1234 <417498134@qq.com>
2026-05-27 05:16:06 +1200

    Update

--- M2D_explore_dctsf_cifar10.py
+++ M2D_explore_dctsf_cifar10.py
@@ -213,8 +213,8 @@ class Proposed_attack():
 
 
     def manifold_search_2d(self, x_o, x_b,
-                           alpha=0.97,
-                           beta=math.pi / 40,
+                           alpha=0.99,
+                           beta=math.pi / 12,
                            beta_min=math.pi / 1000,
                            n=None,
                            u=None):
